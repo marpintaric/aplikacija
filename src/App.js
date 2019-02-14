e2993a8ebbad216160a85f99eb7c7bd3d8e55e2c
Marta Pintarić <mpintaric@Martas-MacBook-Pro.local>
2019-02-14 15:50:49 +0100

    dodavanje registracije

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import login from './components/login';
+import {BrowserRouter  as Router, Route} from 'react-router-dom';
+
 import './App.css';
+import registration from './components/registration';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+        <Route exact path="/" component={login}/>
+        <Route path="/login" component={login}/>
+        <Route path="/register" component={registration} />
+        </div>
+      </Router>
+      
     );
   }
 }
